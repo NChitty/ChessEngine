9954cb162eae69ff52d2e0239989ac9baf742fa2
NChitty <nicholas.chitty@gmail.com>
2021-06-29 16:30:57 -0400

    First

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Chessboard from "chessboardjsx";
+import {ChessInstance, Piece, ShortMove} from "chess.js";
+import Engine from "./engine";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Chess = require("chess.js");
+
+const App: React.FC = () => {
+    const [chess] = useState<ChessInstance>(
+        new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    );
+
+    const [fen, setFen] = useState(chess.fen());
+    const engine = new Engine(chess);
+
+    const handleMove = (move: ShortMove) => {
+        chess.move(move);
+        let start = Date.now();
+        let evaluation = engine.evaluateCurrentPosition();
+        let end = Date.now();
+        console.log(evaluation + " " + (end-start));
+        setFen(chess.fen());
+    };
+
+    return (
+        <div className="flex-center">
+            <Chessboard
+                width={400}
+                position={fen}
+                onDrop={(move) =>
+                    handleMove({
+                        from: move.sourceSquare,
+                        to: move.targetSquare,
+                        promotion: "q",
+                    })
+                }
+            />
+        </div>
+    );
+};
 
 export default App;
